a4a15d1544a3ab95498fe5fd5adaa3827290f1d7
Simone Tonelli <50499875+simotone97@users.noreply.github.com>
2024-09-16 12:27:55 +0200

    Added possibility to download files

--- GitHub_Repos_Scraping.py
+++ GitHub_Repos_Scraping.py
@@ -1,17 +1,18 @@
 import requests
+import os
 
-# Setup your GitHub token here
-GITHUB_TOKEN = 'your_github_token_here'
+# Set your GitHub token here
+GITHUB_TOKEN = 'TOKEN_HERE'
 BASE_API_URL = 'https://api.github.com'
 
 def get_repositories(username):
-    """return the repos list of a user."""
+    """Return the repos list of a user."""
     url = f'{BASE_API_URL}/users/{username}/repos'
     headers = {
         'Authorization': f'token {GITHUB_TOKEN}'
     }
     response = requests.get(url, headers=headers)
-    
+
     if response.status_code == 200:
         return response.json()
     else:
@@ -19,13 +20,13 @@ def get_repositories(username):
         return []
 
 def get_files_in_repository(username, repo_name):
-    """return the list of files in a repository."""
+    """Return the list of files in a repository."""
     url = f'{BASE_API_URL}/repos/{username}/{repo_name}/git/trees/main?recursive=1'
     headers = {
         'Authorization': f'token {GITHUB_TOKEN}'
     }
     response = requests.get(url, headers=headers)
-    
+
     if response.status_code == 200:
         return response.json().get('tree', [])
     else:
@@ -35,21 +36,48 @@ def get_files_in_repository(username, repo_name):
 def get_raw_links(username):
     repos = get_repositories(username)
     raw_links = []
-    
+
     for repo in repos:
         repo_name = repo['name']
         files = get_files_in_repository(username, repo_name)
-        
+
         for file in files:
-            if file['type'] == 'blob':  # solo file, non cartelle
+            if file['type'] == 'blob':  # Solo file, non cartelle
                 raw_url = f'https://raw.githubusercontent.com/{username}/{repo_name}/main/{file["path"]}'
                 raw_links.append(raw_url)
-    
+
     return raw_links
+
+def download_file(url, save_path):
+    """Download a file from a URL and save it locally."""
+    response = requests.get(url)
+    if response.status_code == 200:
+        with open(save_path, 'wb') as file:
+            file.write(response.content)
+        print(f"File downloaded: {save_path}")
+    else:
+        print(f"Failed to download {url}: {response.status_code}")
 
 # Example of usage
 if __name__ == "__main__":
-    github_username = 'username_here' #insert here the GitHub username you want to scratch
+    github_username = 'USERNAME_HERE'  # Insert here the GitHub username you want to scrape
+    raw_links = get_raw_links(github_username)
+
+    for link in raw_links:
+        print(link)
+
+    user_approval = input("Do you want to download these files? (yes/no): ").strip().lower()
+
+    if user_approval == 'yes':
+        save_directory = "downloaded_files"
+        os.makedirs(save_directory, exist_ok=True)
+
+        for link in raw_links:
+            filename = os.path.join(save_directory, link.split('/')[-1])
+            download_file(link, filename)
+    else:
+        print("Download aborted.")
+
     raw_links = get_raw_links(github_username)
     
     for link in raw_links:
